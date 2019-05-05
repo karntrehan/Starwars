9f99517f042f37edb34e785ffda1ba98e35e5177
karntrehan <karntrehan@gmail.com>
2019-05-05 12:30:17 +0530

    Initial commit: Setting up modules with dynamic delivery enabled

---
+++ app/src/main/java/com/karntrehan/starwars/StarwarsMainActivity.kt
@@ -0,0 +1,12 @@
+package com.karntrehan.starwars
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StarwarsMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starwars_main)
+    }
+}
---
+++ characters/src/main/java/com/karntrehan/starwars/characters/CharacterActivity.kt
@@ -0,0 +1,21 @@
+package com.karntrehan.starwars.characters
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_character.*
+
+class CharacterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
