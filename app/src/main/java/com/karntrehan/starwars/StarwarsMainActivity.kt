15a1cc37cc6856cc48448172606545443d5b672d
karntrehan <karntrehan@gmail.com>
2019-05-05 13:10:04 +0530

    [ENH] Adding navigation util to navigate to dynamic feature module from base module.

--- app/src/main/java/com/karntrehan/starwars/StarwarsMainActivity.kt
+++ app/src/main/java/com/karntrehan/starwars/StarwarsMainActivity.kt
@@ -2,11 +2,16 @@ package com.karntrehan.starwars
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.karntrehan.starwars.utils.Activities
+import com.karntrehan.starwars.utils.intentTo
 
 class StarwarsMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_starwars_main)
+
+        startActivity(intentTo(Activities.Characters))
+        finish()
     }
 }
---
+++ app/src/main/java/com/karntrehan/starwars/utils/NavigationHelper.kt
@@ -0,0 +1,27 @@
+package com.karntrehan.starwars.utils
+
+import android.content.Intent
+
+/*Inspired from Plaid's ActivityHelper
+https://github.com/android/plaid/blob/776abd95f02b1e34360a3fef149baa7d9d4b23a0/core/src/main/java/io/plaidapp/core/util/ActivityHelper.kt#L1*/
+
+
+private const val PACKAGE_NAME = "com.karntrehan.starwars"
+
+fun intentTo(navigatableActivity: NavigatableActivity): Intent {
+    return Intent(Intent.ACTION_VIEW).setClassName(
+            PACKAGE_NAME,
+            navigatableActivity.className)
+}
+
+interface NavigatableActivity {
+    val className: String
+}
+
+object Activities {
+
+    object Characters : NavigatableActivity {
+        override val className = "$PACKAGE_NAME.characters.CharacterActivity"
+    }
+
+}
