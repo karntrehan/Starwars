7cc5e8747b9233112b5ec0af64947d51f2619899
karntrehan <karntrehan@gmail.com>
2019-07-26 10:45:45 +0530

    [ENH] Make coroutines run in parallel

--- characters/src/main/java/com/karntrehan/starwars/characters/details/CharacterDetailsVM.kt
+++ characters/src/main/java/com/karntrehan/starwars/characters/details/CharacterDetailsVM.kt
@@ -9,11 +9,8 @@ import com.karntrehan.starwars.characters.details.models.SpeciesDetailsModel
 import com.karntrehan.starwars.characters.details.models.response.HomeworldResponseModel
 import com.karntrehan.starwars.extensions.divide
 import com.karntrehan.starwars.extensions.hide
-import com.karntrehan.starwars.extensions.isValid
 import com.karntrehan.starwars.extensions.show
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.lang.Error
+import kotlinx.coroutines.*
 
 class CharacterDetailsVM(private val repo: CharacterDetailsContract.Repo) : BaseVM() {
 
@@ -28,28 +25,35 @@ class CharacterDetailsVM(private val repo: CharacterDetailsContract.Repo) : Base
                 //Get the core details
                 val remoteCoreDetails = repo.getCharacterDetails(url)
                 //Calculate the height in ft and inches
-                var coreDetails = remoteCoreDetails.copy(heightFtInches = cmToFeet(remoteCoreDetails.heightCentimeters))
+                val coreDetails = remoteCoreDetails.copy(heightFtInches = cmToFeet(remoteCoreDetails.heightCentimeters))
                 //Send the core details to the UI
                 characterDetails.postValue(coreDetails)
 
-                //Fire a request to get species and homeworlds
-                val speciesDetails = speciesAndHomeWorld(coreDetails)
-                //Update the core details with species details
-                coreDetails = coreDetails.copy(specieDetails = speciesDetails)
-                //Send the core + specie details to the UI
-                characterDetails.postValue(coreDetails)
-
-                //Fire a request to get films
-                val films = films(coreDetails)
-                //Update core details with film details
-                coreDetails = coreDetails.copy(filmDetails = films)
-                //Send core + film details to the UI
-                characterDetails.postValue(coreDetails)
-
-                _loading.hide()
+                loadAdditionalDetails(coreDetails)
             }
         }
         return characterDetails
+    }
+
+    private suspend fun loadAdditionalDetails(coreDetails: CharacterDetailsModel) {
+        coroutineScope {
+            var combinedDetails = coreDetails
+
+            launchSafely {
+                //Update the core details with species details
+                combinedDetails = combinedDetails.copy(specieDetails = speciesAndHomeWorld(combinedDetails))
+                //Send the core + specie details to the UI
+                characterDetails.postValue(combinedDetails)
+            }
+
+            launchSafely {
+                //Update core details with film details
+                combinedDetails = combinedDetails.copy(filmDetails = films(combinedDetails))
+                //Send core + film details to the UI
+                characterDetails.postValue(combinedDetails)
+                _loading.hide()
+            }
+        }
     }
 
     private suspend fun speciesAndHomeWorld(details: CharacterDetailsModel): List<SpeciesDetailsModel> {
@@ -59,9 +63,12 @@ class CharacterDetailsVM(private val repo: CharacterDetailsContract.Repo) : Base
                 val specieDetails = repo.getSpecieDetails(speciesUrl)
 
                 //If homeworld url is valid, fetch its details, else return an empty object
-                val homeWorldDetails = if (specieDetails.homeworldUrl.isValid())
+                val homeWorldDetails = try {
                     repo.getHomeworldDetails(specieDetails.homeworldUrl)
-                else HomeworldResponseModel()
+                } catch (t: Throwable) {
+                    t.printStackTrace()
+                    HomeworldResponseModel()
+                }
 
                 SpeciesDetailsModel(
                     specieDetails.name, specieDetails.language,
